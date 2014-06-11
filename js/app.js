78f052632db81c768cdd9fe3c946295afbe22d2a
ziaud_deen <shaul21h@gmail.com>
2014-06-11 10:40:42 +0530

    Added Facebook - bare minimal code

--- js/app.js
+++ js/app.js
@@ -7,8 +7,13 @@ var fbmod = angular.module("zangular-facebook",["fbmod"]);
 fbmod.controller('mainCtrl', [
 	'$scope', 'zfacebook', function($scope, zfacebook){
 		
-		zfacebook();
-		
+		zfacebook.init({
+			appid: "<your-app-id>"
+		}).then(function(fb){
+			fb.login(function(response){
+				console.log(response);
+			}, {scope: [zfacebook.extendedpermissions.email, zfacebook.permissions.userbirthday]});
+		});
 	}]);
 
 
--- js/zfacebook.js
+++ js/zfacebook.js
@@ -1,4 +1,85 @@
 angular.module('fbmod',[])
-	.factory('zfacebook', function(){
-		return "hello world";
-	});
+	.factory('zfacebook', [
+	'$q',
+	
+	function($q){
+			(function(d, s, id){ //loads fb 
+					 var js, fjs = d.getElementsByTagName(s)[0];
+					 if (d.getElementById(id)) {return;}
+					 js = d.createElement(s); js.id = id;
+					 js.src = "//connect.facebook.net/en_US/sdk.js";
+					 fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+			
+			//permissions
+			var permissions = {
+				useraboutme: "user_about_me",
+				useractionsnews: "user_actions.news",
+				userbirthday:"user_birthday",
+				userfriends: "user_friends",
+				userhometown: "user_hometown",
+				userlocation: "user_location",
+				userrelationships: "user_relationships",
+				usertaggedplaces: "user_tagged_places",
+				userworkhistory: "user_work_history",
+				useractionsbooks: "user_actions.books",
+				useractionsvideo: "user_actions.video",
+				usereducationhistory: "user_education_history",
+				usergamesactivity: "user_games_activity",
+				userinterests: "user_interests",
+				userphotos: "user_photos",
+				userreligionpolitics: "user_religion_politics",
+				uservideos: "user_videos",
+				useractionsmusic: "user_actions.music",
+				useractivities: "user_activities",
+				userevents: "user_events",
+				usergroups: "user_groups",
+				userlikes: "user_likes",
+				userrelationshipdetails: "user_relationship_details",
+				userstatus: "user_status",
+				userwebsite: "user_website"
+			};
+			
+			//extended permissions
+			var extendedpermissions = {
+				email : "email",
+				publishactions: "publish_actions",
+				readmailbox: "read_mailbox",
+				rsvpevent: "rsvp_event",
+				managenotifications: "manage_notifications",
+				readfriendlists: "read_friendlists",
+				readpagemailboxes: "read_page_mailboxes",
+				managepages: "manage_pages",
+				readinsights: "read_insights",
+				readstream: "read_stream"
+			};
+			
+			var isFbReady = function(){
+				var defer = $q.defer();
+				var checkfb = setInterval(function(){
+					if (angular.isDefined(FB)) {
+						clearInterval(checkfb);
+						defer.resolve(FB);
+					}
+				},100)
+				return defer.promise;
+			}
+			var init = function(settings){
+				var defer = $q.defer();
+				isFbReady().then(function(fb){
+					fb.init({
+          appId      : settings.appid,
+          xfbml      : true,
+          version    : 'v2.0'
+					});
+					
+					defer.resolve(fb);
+				})
+				return defer.promise;
+			}
+			return {
+				init : init,
+				permissions : permissions,
+				extendedpermissions : extendedpermissions
+			}
+	}]);
